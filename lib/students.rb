fedc11a07b6c121b4d72d87eb52c9bfe9e012aff
tgray017 <tgray017@gmail.com>
2019-01-21 15:56:38 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,9 +13,13 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
+  "
+  SELECT AVG(gpa) FROM students;
+  "
 end
 
 def total_tardies_for_all_students
+  
 end
 
 def average_gpa_for_9th_grade
